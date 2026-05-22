0ead3af7529d1bb15c180ba1204414ac60135b1a
Soumadip <39thdecember@gmail.com>
2026-05-23 00:46:06 +0530

    Add models

---
+++ internal/models/user.model.go
@@ -0,0 +1,46 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        uint     `json:"id" gorm:"primaryKey"`
+	Email     string   `json:"email" gorm:"uniqueIndex;not null"`
+	Password  string   `json:"-" gorm:"not null"`
+	FirstName string   `json:"first_name" gorm:"not null"`
+	LastName  string   `json:"last_name" gorm:"not null"`
+	Phone     string   `json:"phone"`
+	IsActive  bool     `json:"is_active" gorm:"default:true"`
+	Role      UserRole `json:"role" gorm:"default:customer"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
+
+	// Relationships
+	RefreshTokens []RefreshToken `json:"-"`
+	Orders []Order `json:"-"`
+ 	Cart  Cart `json:"-"`
+}
+
+type UserRole string
+
+const (
+	UserRoleCustomer UserRole = "customer"
+	UserRoleAdmin UserRole = "admin"
+)
+
+// refresh token
+type RefreshToken struct {
+	ID uint `json:"id" gorm:"primaryKey"`
+	UserID uint `json:"user_id" gorm:"not null"`
+	Token string `json:"token" gorm:"uniqueIndex;not null"`
+	Expiry time.Time `json:"expiry" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
+
+	// relationships
+	User User `json:"-"`
+}
